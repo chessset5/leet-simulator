51cae5eca136a4211c57c78958a50e120331a0e8
Aaron Shackelford <grayjackel5@gmail.com>
2026-01-23 19:46:11 -0800

    adding debug case

--- run_leet.py
+++ run_leet.py
@@ -3,7 +3,12 @@ single runner for leet, use for debugging
 """
 
 from leet import Solution
+from LeetTypes.single_list_node import ListNode, list_to_node
 
 # add input below
 
 s = Solution()
+
+head: ListNode | None = list_to_node([1, 2, 3, 4])
+
+head = s.swapPairs(head=head)
