212d4d948cc4f281329fcd73c5cc21476a068ab8
Aaron Shackelford <grayjackel5@gmail.com>
2026-01-28 15:39:50 -0800

    renaming strIndex to vis_index to account for more than just string visualizations of the current selected index

---
+++ MyTypes/debug_rep/vis_index.py
@@ -0,0 +1,33 @@
+"""
+Docstring for MyTypes.debug_rep.vis_index
+
+Visual Index of a value and its selected index
+"""
+
+
+class StrIndex:
+    """
+    visually shows off index of a string
+    """
+
+    def __init__(self, string: str, index: int) -> None:
+        self.string: str = string
+        self.index: int = index
+
+    def __str__(self) -> str:
+        return f"{self.index} | {self.string}"
+
+    def __repr__(self) -> str:
+        i: int = self.index
+        i %= len(self.string)
+
+        return self.string[:i] + "[" + self.string[i] + "]" + self.string[i + 1 :]
+
+
+class BinIntIndex(StrIndex):
+    """
+    visually shows off index of a binary string
+    """
+
+    def __init__(self, integer: int, index: int) -> None:
+        super().__init__(bin(integer), index + 2)
